568c98d89bb6b954ba003e3e210501ec99153365
Cauã Rufino de Sá <cauarufinodesa@gmail.com>
2024-04-23 09:51:19 -0300

    Add project files.

---
+++ Ex10-5by5-Fibonacci/Program.cs
@@ -0,0 +1,22 @@
+﻿int numberOne, numberTwo, aux, max;
+
+do
+{
+    numberOne = 1;
+    numberTwo = -1;
+    aux = 1;
+
+    max = int.Parse(Console.ReadLine());
+
+    for (int i = 0; i < max; i++)
+    {
+        aux = numberTwo;
+        numberTwo = numberOne;
+        numberOne += aux;
+        Console.Write($"{numberOne} ");
+    }
+
+    Console.WriteLine();
+} while (max != 0);
+
+Console.ReadKey();
